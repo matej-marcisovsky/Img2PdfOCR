cdb129dc057b956350e3fe3904aaea9669bde0bc
Matěj Marčišovský <matej@marcisovsky.cz>
2025-08-19 11:15:19 +0200

    add zip

--- downloaders/digiport.js
+++ downloaders/digiport.js
@@ -1,18 +1,36 @@
+const { BlobWriter, ZipWriter } = await import('https://deno.land/x/zipjs/index.js');
+
 const { group, page_count, publication } = JSON.parse(document.getElementById('root').getAttribute('data-config'));
+const zipFileWriter = new BlobWriter();
+const zipWriter = new ZipWriter(zipFileWriter);
 
 async function downloadPage(page) {
-	const data = await fetch(`https://cdn2.reader.digiport.cz/gluster/${group}/${publication}/high/bg${page.toString(16)}.jpg?v0`, { credentials: 'include' });
-	const blob = await data.blob();
+	const data = await fetch(`https://cdn7.reader.digiport.cz/gluster/${group}/${publication}/high/bg${page.toString(16)}.jpg?v0`, { credentials: 'include' });
+	const filename = `bg${`${page}`.padStart(3, '0')}.jpg`;
 
+	if (data.status !== 200) {
+		throw new Error(`Failed to fetch page ${page}`);
+	}
+
+	console.log(filename);
+
+	await zipWriter.add(filename, data.body);
+}
+
+try {
+	for (let page = 1; page <= page_count; page++) {
+		await downloadPage(page);
+	}
+	
+	await zipWriter.close();
+	
 	const a = document.createElement('a');
-	a.href = window.URL.createObjectURL(blob);
-	a.download = `bg${`${page}`.padStart(3, '0')}.jpg`;
-	a.click();
-	a.remove();
+		a.href = window.URL.createObjectURL(await zipFileWriter.getData());
+		a.download = `magazine.zip`;
+		a.click();
+		a.remove();
+	
+	console.log(`Downloaded ${page_count} pages.`);
+} catch (error) {
+	console.error(error);
 }
-
-for (let page = 1; page <= page_count; page++) {
-	await downloadPage(page);
-}
-
-console.log(`Downloaded ${page_count} pages.`);
